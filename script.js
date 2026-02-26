66346d8f759804667a257c4254dc3eb48c0a446b
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-02-26 22:46:52 +0530

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Update values dynamically
+
+let rooms = 5;
+let bookings = 3;
+let guests = 4;
+
+document.getElementById("rooms").innerText = rooms;
+document.getElementById("bookings").innerText = bookings;
+document.getElementById("guests").innerText = guests;
