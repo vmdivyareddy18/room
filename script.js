76a22fddce0df4531b07b43b8182e4a38705e395
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-03-02 22:26:31 +0530

    update

--- script.js
+++ script.js
@@ -3,7 +3,3 @@
 let rooms = 5;
 let bookings = 3;
 let guests = 4;
-
-document.getElementById("rooms").innerText = rooms;
-document.getElementById("bookings").innerText = bookings;
-document.getElementById("guests").innerText = guests;
