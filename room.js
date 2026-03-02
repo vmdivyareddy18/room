59bf82d1ba170fef9398c33fe608da235cb5df6d
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-03-02 21:56:00 +0530

    update

--- room.js
+++ room.js
@@ -1,3 +1,27 @@
 function bookRoom(roomName) {
     alert("Booking confirmed for " + roomName);
 }
+
+function openModal() {
+    document.getElementById("bookingModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("bookingModal").style.display = "none";
+}
+
+// Close when clicking outside
+window.onclick = function (event) {
+    let modal = document.getElementById("bookingModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Form Submit
+document.getElementById("bookingForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Room Booked Successfully!");
+    closeModal();
+});
+
